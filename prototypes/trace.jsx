0a126c1f1fb95cc9985778a27a8bc3b499a58a8c
GalaGuard <270186452+GalaGuard@users.noreply.github.com>
2026-05-22 11:18:51 -0700

    Finish test image trace and expand.
    Add test image.

--- prototypes/trace.jsx
+++ prototypes/trace.jsx
@@ -1,6 +1,27 @@
-
 function traceImage() {
-
+    if (app.documents.length == 0) {
+        alert("Open a document first");
+        return;
+    }
+    var doc = app.activeDocument;
+    var sel = doc.selection;
+    
+    for (var i = 0; i < sel.length; i++) {
+        var item = sel[i];
+        if (item.typename !== "RasterItem") {
+            alert("Selected items must be RasterItem, instead are " + item.typename);
+            return;
+        }
+        var traceObj = item.trace().tracing //grabbing the trace object
+        var traceOpt = traceObj.tracingOptions;
+        traceOpt.threshold = 210
+        traceOpt.tracingMode = TracingModeType.TRACINGMODEBLACKANDWHITE;
+        traceOpt.ignoreWhite = true;
+        item.tracingMethod = TracingMethodType.TRACINGMETHODABUTTING;
+        app.redraw();
+        traceObj.expandTracing();
+    }
+    alert("finished");
 }
 
 traceImage();
